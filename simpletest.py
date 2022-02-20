658abe2bfd2bc8e24001011deac2738a00ee0450
RooftopMadness <83479077+RooftopMadness@users.noreply.github.com>
2022-02-20 23:05:51 +0100

    Update simpletest.py

--- simpletest.py
+++ simpletest.py
@@ -6,5 +6,5 @@ SPI_PORT   = 0
 SPI_DEVICE = 2
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 while(True):
-	print("CH0 "  + str(mcp.read_adc(0)) + "CH1 " + str(mcp.read_adc(1)))
+	print("CH0: "  + str(mcp.read_adc(0)) + " - CH1: " + str(mcp.read_adc(1)))
 	time.sleep(0.5)
